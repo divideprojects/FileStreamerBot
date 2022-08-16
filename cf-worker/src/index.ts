2dc9b257233df123fd5f480659ee4411603c0db3
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-08-15 22:46:32 -0700

    add worker to fetch file from source

---
+++ cf-worker/src/index.ts
@@ -0,0 +1,28 @@
+
+import { Context, Hono } from "hono";
+
+// create a new hono client
+const app = new Hono();
+
+// listen for get requests on /
+app.get("/*", async (c: Context) => {
+    const formedUrl = new URL(c.req.url);
+    let fqdn = c.env.FQDN;
+    if (!fqdn.endsWith("/")) fqdn = fqdn + "/";
+    const dlUrl = fqdn + formedUrl.pathname.slice(1);
+
+    // Fetch from origin server.
+    const response = await fetch(dlUrl);
+
+    // Create an identity TransformStream (a.k.a. a pipe).
+    // The readable side will become our new response body.
+    const { readable, writable } = new TransformStream();
+
+    // Start pumping the body. NOTE: No await!
+    response.body?.pipeTo(writable);
+
+    // ... and deliver our Response while that’s running.
+    return new Response(readable, response);
+});
+
+export default app;
