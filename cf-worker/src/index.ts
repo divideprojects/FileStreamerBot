561185c066decdb240097ec4dcf76e204b4ab47c
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-08-20 17:18:08 -0700

    remove arc.io

--- cf-worker/src/index.ts
+++ cf-worker/src/index.ts
@@ -3,21 +3,6 @@ import { Context, Hono } from "hono";
 // create a new hono client
 const app = new Hono();
 
-app.get("/arc-sw.js", async (c: Context) => {
-    // Fetch from origin server.
-    const response = await fetch(c.env.ARC_CORE);
-
-    // Create an identity TransformStream (a.k.a. a pipe).
-    // The readable side will become our new response body.
-    const { readable, writable } = new TransformStream();
-
-    // Start pumping the body. NOTE: No await!
-    response.body?.pipeTo(writable);
-
-    // ... and deliver our Response while that’s running.
-    return new Response(readable, response);
-});
-
 // listen for get requests on /
 app.get("/*", async (c: Context) => {
     const formedUrl = new URL(c.req.url);
