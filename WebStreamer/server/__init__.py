2f98c741a139342b86bf61d2294c047a09d6d8ea
Divanshu Chauhan <divkix@divkix.me>
2023-06-11 11:36:08 +0530

    remove jinja2

--- WebStreamer/server/__init__.py
+++ WebStreamer/server/__init__.py
@@ -1,6 +1,4 @@
 from aiohttp import web
-from aiohttp_jinja2 import setup as setup_jinja2
-from jinja2 import FileSystemLoader
 
 from WebStreamer.server.stream_routes import routes
 
@@ -12,12 +10,6 @@ def web_server() -> web.Application:
         web.Application: The web server
     """
     web_app = web.Application(client_max_size=30000000)
-    # setup jinja2 with the web templates from templates folder
-    setup_jinja2(
-        web_app,
-        enable_async=True,
-        loader=FileSystemLoader("/app/WebStreamer/html/templates"),
-    )
     # add the routes to the web app
     web_app.add_routes(routes)
     return web_app
